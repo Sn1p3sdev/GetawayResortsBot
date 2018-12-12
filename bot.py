909468e2ae9c06556251d8068543ec824883c2d8
Sn1p3sdev <36523860+Sn1p3sdev@users.noreply.github.com>
2018-12-12 17:43:59 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import os, discord
-from discord.ext import commands
+import discord
 import asyncio
-from itertools import cycle
 import json
+from discord.ext import commands
+from itertools import cycle
 
 #settings or values
 
@@ -45,45 +45,45 @@ async def on_reaction_add():
 	await client.send_
 
 #LvL System
-@client.event
-async def on_member_join():
-	with open ('users.json', 'r') as f:
-		users = json.load(f)
-
-		await update_data(users, message.author)
-
-	with open('users.json', 'w') as f:
-		users = json.load(f)
-
-@client.event
-async def on_message(message):
-	with open('users.json', 'r') as f:
-		users = json.load(f)
-
-	await update_data(users, message.author)
-	await add_experience(users, message.author, 5)
-	await level_up(users, message.author, message.channel)
-
-	with open('users.json', 'w') as f:
-		json.dump(users, f)
-
-async def update_data(users, user):
-	if not user.id in users:
-		users[user.id] = {}
-		users[user.id]['experience'] = 0
-		users[user.id]['level'] = 1
-
-async def add_experience(users, user, exp):
-	users[user.id]['experience'] += exp
-
-async def level_up(users, user, channel):
-	experience = users[user.id]['experience']
-	lvl_start = users[user.id]['level']
-	lvl_end = int(experience ** (1/4))
-
-	if lvl_start < lvl_end:
-		await client.send_message(channel, '{} has leveled up to level {}!!! Congrats {}!!!'.format(user.mention, lvl_end, user.mention))
-		users[user.id]['level'] = lvl_end
+#@client.event
+#async def on_member_join():
+#	with open ('users.json', 'r') as f:
+#		users = json.load(f)
+#
+#		await update_data(users, message.author)
+#
+#	with open('users.json', 'w') as f:
+#		users = json.load(f)
+#
+#@client.event
+#async def on_message(message):
+#	with open('users.json', 'r') as f:
+#		users = json.load(f)
+#
+#	await update_data(users, message.author)
+#	await add_experience(users, message.author, 5)
+#	await level_up(users, message.author, message.channel)
+#
+#	with open('users.json', 'w') as f:
+#		json.dump(users, f)
+#
+#async def update_data(users, user):
+#	if not user.id in users:
+#		users[user.id] = {}
+#		users[user.id]['experience'] = 0
+#		users[user.id]['level'] = 1
+#
+#async def add_experience(users, user, exp):
+#	users[user.id]['experience'] += exp
+#
+#async def level_up(users, user, channel):
+#	experience = users[user.id]['experience']
+#	lvl_start = users[user.id]['level']
+#	lvl_end = int(experience ** (1/4))
+#
+#	if lvl_start < lvl_end:
+#		await client.send_message(channel, '{} has leveled up to level {}!!! Congrats {}!!!'.format(user.mention, lvl_end, user.mention))
+#		users[user.id]['level'] = lvl_end
 
 
 #C O M M A N D S ! ! !
