44d1e5806a4065a566031b945827de3590e34aec
Sn1p3sdev <36523860+Sn1p3sdev@users.noreply.github.com>
2018-12-14 10:01:04 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def clear(ctx, amount=1000):
 @client.command(pass_context=True)
 async def mute(ctx, member: discord.Member):
 	 if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
-		role = discord.utils.get(member.server.roles, name='Muted')
+		role = discord.utils.get(member.server.roles, name='G.R Muted')
 		await bot.add_roles(member, role)
 		embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
 		await bot.say(embed=embed)
